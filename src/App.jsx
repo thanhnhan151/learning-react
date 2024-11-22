b3fd43a6eb2fd7462620dcb37c5600bc196e6311
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-11-22 17:05:09 +0700

    Event Handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,15 @@
+import { LoginForm } from "./components/LoginForm";
+
 export default function App() {
-  // NOT_VERIFIED | VERIFIED | ACCOUNT_DISABLED
-  const USER_STATUS = 'VERIFIED'
 
-  switch (USER_STATUS){
-    case 'NOT_VERIFIED':
-      return (
-        <div>
-          <span>
-            You are not verified
-          </span>
-        </div>
-      )
-    case 'VERIFIED':
-      return (
-        <div>
-          <span>
-            You are verified
-          </span>
-        </div>
-      )
-    case 'ACCOUNT_DISABLED':
-      return (
-        <div>
-          <span>
-            Your account is disabled
-          </span>
-        </div>
-      )
-    default:
-      return (
-        <div>
-          <span>
-            Please contact the system admin
-          </span>
-        </div>
-      )
-  }
+  window.addEventListener("resize", () => {
+    console.log(window.innerHeight, window.innerWidth)
+  })
 
-  // if (USER_STATUS === 'NOT_VERIFIED') {
-  //   return (
-  //     <div>
-  //       <span>
-  //         You are not verified. Please verify your Email or Mobile Number.
-  //         </span>
-  //     </div>
-  //   )
-  // } else if (USER_STATUS === 'VERIFIED') {
-  //   return (
-  //     <div>
-  //       <span>
-  //         You are verified. Congrats! Click here to Access Your Dashboard.
-  //       </span>
-  //     </div>
-  //   )
-  // } else if (USER_STATUS === 'ACCOUNT_DISABLED') {
-  //   return (
-  //     <div>
-  //       <span>
-  //         Your account is disabled
-  //       </span>
-  //     </div>
-  //   )
-  // } else {
-  //   return (
-  //     <div>
-  //       <span>
-  //         Please contact the system admin
-  //       </span>
-  //     </div>
-  //   )
-  // }
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
 }
 
