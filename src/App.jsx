9f1947b90874727bfec8e9d1c79f272d69766883
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-11-25 16:11:11 +0700

    Edit, Delete, Add Using State

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-import { RegisterForm } from "./components/RegisterForm";
+import { useState } from "react";
+import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [counter, setCounter] = useState(3)
 
-  window.addEventListener("resize", () => {
-    console.log(window.innerHeight, window.innerWidth)
-  })
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "kevin",
+      email: "kevin@mail.com"
+    },
+    {
+      id: 2,
+      username: "ben",
+      email: "benn@mail.com"
+    }
+  ])
 
   return (
     <div>
-      <RegisterForm />
+      <form onSubmit={e => {
+        e.preventDefault()
+        const newUser = {
+          id: counter,
+          username,
+          email
+        }
+        setCounter((currentCounter) => currentCounter + 1)
+        setUsers((currentUsersState) => [...currentUsersState, newUser])
+      }}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input 
+          name="username" 
+          id="username" 
+          value={username} 
+          onChange={(e) => {
+            setUsername(e.target.value)
+          }}/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input 
+          name="email" 
+          id="email" 
+          value={email} 
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}/>
+        </div>
+        <button>Add User</button>
+      </form>
+      <br />
+      {users.map((user) => <UserDetails key={user.id} user={user} setUsers={setUsers} />)}    
     </div>
   )
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,17 +1,59 @@
 import PropTypes from "prop-types"
+import { useState } from "react"
 
-export function UserDetails({ user }) {
+export function UserDetails({ user, setUsers }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState(user.username)
+  const [email, setEmail] = useState(user.email)
+
   return (
-  <div>
-    <b>ID: </b>
-    <span>{user.id}</span>
-    <br />
-    <b>Username: </b>
-    <span>{user.username}</span>
-    <br />
-    <b>Email: </b>
-    <span>{user.email}</span>
-  </div>)
+    <div>
+      <div>
+        <button onClick={() => {
+          setIsEditing((currentState) => !currentState)
+        }}>Edit</button>
+        <button 
+        onClick={() => {
+          setUsers((currentUsersState) => currentUsersState.filter(
+            (currentUser) => currentUser.id !== user.id
+          ))
+        }}>Delete</button>
+        {isEditing && (<button 
+        onClick={() => {
+          setUsers((currentUsersState) =>
+              currentUsersState.map((currentUser) => 
+                currentUser.id === user.id 
+                  ? ({ ...currentUser, username, email }) 
+                  : currentUser
+              )         
+          )
+          setIsEditing(false)
+        }}>Save</button>)}
+      </div>
+      <div>
+        <b>ID: </b>
+        <span>{user.id}</span>
+        <br />
+        <b>Username: </b>
+        {isEditing ? <input 
+        name="username" 
+        id="username" 
+        value={username} 
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }} /> : <span>{user.username}</span>}
+        <br />
+        <b>Email: </b>
+        {isEditing ? <input 
+        name="email" 
+        id="email" 
+        value={email} 
+        onChange={(e) => {
+          setEmail(e.target.value)
+        }} /> : <span>{user.email}</span>}
+      </div>     
+    </div>
+  )
 }
 
 UserDetails.propTypes = {
@@ -19,5 +61,6 @@ UserDetails.propTypes = {
     id: PropTypes.number.isRequired,
     username: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired
-  })
+  }).isRequired,
+  setUsers: PropTypes.func.isRequired
 }
