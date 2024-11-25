fd11c51f7f98c35b71d8bab8f5b7f78861f0e72a
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-11-25 16:35:57 +0700

    useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
-import { useState } from "react";
-import { UserDetails } from "./components/UserDetails";
+import { useState, useEffect } from "react";
+// import { UserDetails } from "./components/UserDetails";
 
 export default function App() {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [counter, setCounter] = useState(3)
-
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: "kevin",
-      email: "kevin@mail.com"
-    },
-    {
-      id: 2,
-      username: "ben",
-      email: "benn@mail.com"
-    }
-  ])
+  // const [username, setUsername] = useState("")
+  // const [email, setEmail] = useState("")
+  const [counter, setCounter] = useState(0)
+  const [sync, setSync] = useState(false)
+  useEffect(() => {
+    console.log("Rendering...")
+    document.title = "React Tutorial " + counter
+  }, [sync])
+  // const [users, setUsers] = useState([
+  //   {
+  //     id: 1,
+  //     username: "kevin",
+  //     email: "kevin@mail.com"
+  //   },
+  //   {
+  //     id: 2,
+  //     username: "ben",
+  //     email: "benn@mail.com"
+  //   }
+  // ])
 
   return (
     <div>
-      <form onSubmit={e => {
+      <div>You clicked the button {counter} times</div>
+      <button onClick={() => {
+        setCounter((count) => count + 1)
+      }} >CLick Me</button>
+      <button onClick={() => setSync((current) => !current)}></button>
+      {/* <form onSubmit={e => {
         e.preventDefault()
         const newUser = {
           id: counter,
@@ -54,7 +63,7 @@ export default function App() {
         <button>Add User</button>
       </form>
       <br />
-      {users.map((user) => <UserDetails key={user.id} user={user} setUsers={setUsers} />)}    
+      {users.map((user) => <UserDetails key={user.id} user={user} setUsers={setUsers} />)} */}
     </div>
   )
 }
